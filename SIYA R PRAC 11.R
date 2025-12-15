c09ecb3b3bbbb5ed83003ccb0271cfec1a92322d
Siya <poipkarsiyacs242557@gmail.com>
2025-12-16 01:09:34 +0530

    Enhance data processing for Spotify dataset
    Added data manipulation steps to reshape the Spotify dataset using pivot_longer and pivot_wider functions.

--- SIYA R PRAC 11.R
+++ SIYA R PRAC 11.R
@@ -3,3 +3,23 @@ library(tidyr)
 library(dplyr)
 spotify <- read.csv("C:/Users/itlab/OneDrive/Documents/S105/DATASET/spotify_data clean.csv")
 head(spotify)
+df <- spotify %>%
+  select(track_name, track_popularity, artist_popularity)
+
+head(df)
+df_long <- df %>%
+  pivot_longer(
+    cols = c(track_popularity, artist_popularity),
+    names_to = "subject",
+    values_to = "score"
+  )
+
+df_long
+df_wide <- df_long %>%
+  pivot_wider(
+    names_from = subject,
+    values_from = score
+  )
+
+df_wide
+print(head(category_pivot))
