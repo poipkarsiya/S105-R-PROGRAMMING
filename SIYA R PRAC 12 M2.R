2f8bd1b71894a0a12a31527a455719b847a5e0e4
Siya <poipkarsiyacs242557@gmail.com>
2026-01-22 14:02:01 +0530

    Delete SIYA R PRAC 12 M2.R

--- SIYA R PRAC 12 M2.R
+++
@@ -1,8 +0,0 @@
-print("Siya Poipkar S105")
-mall <- read.csv("C:/Users/DELL/Desktop/siya/DATASET/Mall_Customers.csv")
-
-mall_numeric <- mall[, c("Age", "Annual.Income..k..", "Spending.Score..1.100.")]
-
-cor_matrix <- cor(mall_numeric)
-
-print(cor_matrix)
